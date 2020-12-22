fcb55a6250202451c3c16d4c08bafc56c48b0b58
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-12-22 13:14:36 +0100

    per tests progress

--- perf/http_client.py
+++ perf/http_client.py
@@ -15,6 +15,7 @@ else:
 import multiprocessing
 import functools
 import itertools
+from utils import progress
 
 
 class HttpClient(object):
@@ -22,21 +23,29 @@ class HttpClient(object):
   def __do(self, reqs, on_work=lambda *args: None, on_result=lambda *args: None, on_panic=lambda *args: None):
     total_success = 0
     total_errors = list()
-
-    total_requests = len(reqs)
+    total_progress = 0
+    total_work = len(reqs)
     chunk_size = 1000
 
-    for offset in range(0, total_requests, chunk_size):
+    progress('{1:.2f}% of {0}'.format(total_work, 0))
+
+    for offset in range(0, total_work, chunk_size):
       work = reqs[offset:offset+chunk_size]
       if not work:
         continue
 
-      futures = multiprocessing.Pool(processes=4).map_async(on_work, work).get()
+      futures = multiprocessing.Pool(processes=10).map_async(on_work, work).get()
       (results, errors) = functools.reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), futures)
+
+      total_progress += len(work)
+
+      progress('{1:.2f}% of {0}'.format(total_work, 100 * (total_progress/total_work)))
 
       total_success += len(results)
       for result in results:
         on_result(*result)
+
+    progress('{1:.2f}% of {0}'.format(total_work, 100))
 
     return total_success, total_errors
 
