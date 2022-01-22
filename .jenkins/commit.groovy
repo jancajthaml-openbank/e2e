9956ff856a46bbcaf1c85a4edd8caad3e20d4509
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-01-22 09:50:21 +0100

    jenkins ci

---
+++ .jenkins/commit.groovy
@@ -0,0 +1,105 @@
+def artifactory = Artifactory.server "artifactory"
+
+pipeline {
+
+    agent {
+        label 'docker'
+    }
+
+    options {
+        skipDefaultCheckout(true)
+        ansiColor('xterm')
+        buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+        disableConcurrentBuilds()
+        disableResume()
+        timeout(time: 10, unit: 'MINUTES')
+        timestamps()
+    }
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                script {
+                    currentBuild.displayName = "#${currentBuild.number} - ?"
+                }
+                deleteDir()
+                checkout(scm)
+            }
+        }
+
+        stage('Setup') {
+            steps {
+                script {
+                    env.GIT_BRANCH = sh(
+                        script: 'git name-rev --name-only HEAD',
+                        returnStdout: true
+                    ).trim() - 'remotes/origin/'
+                    env.ARCH = sh(
+                        script: 'dpkg --print-architecture',
+                        returnStdout: true
+                    ).trim()
+
+                    currentBuild.displayName = "#${currentBuild.number} - ${env.GIT_BRANCH}"
+                }
+            }
+        }
+
+        stage('BlackBox Test') {
+            agent {
+                docker {
+                    image "jancajthaml/bbtest:${env.ARCH}"
+                    args """-u 0"""
+                    reuseNode true
+                }
+            }
+            options {
+                timeout(time: 5, unit: 'MINUTES')
+            }
+            steps {
+                script {
+                    cid = sh(
+                        script: 'hostname',
+                        returnStdout: true
+                    ).trim()
+                    options = """
+                        |-e CI=true
+                        |--volumes-from=${cid}
+                        |-v /var/run/docker.sock:/var/run/docker.sock:rw
+                        |-v /var/lib/docker/containers:/var/lib/docker/containers:rw
+                        |-v /sys/fs/cgroup:/sys/fs/cgroup:ro
+                        |-u 0
+                    """.stripMargin().stripIndent().replaceAll("[\\t\\n\\r]+"," ").stripMargin().stripIndent()
+                    docker.image("jancajthaml/bbtest:${env.ARCH}").withRun(options) { c ->
+                        sh "docker exec -t ${c.id} python3 ${env.WORKSPACE}/bbtest/main.py"
+                    }
+                }
+            }
+        }
+
+    }
+
+    post {
+        always {
+            script {
+                cucumber(
+                    reportTitle: 'Black Box Test',
+                    fileIncludePattern: '*',
+                    jsonReportDirectory: "${env.WORKSPACE}/reports/blackbox-tests/cucumber"
+                )
+            }
+        }
+        success {
+            cleanWs()
+        }
+        failure {
+            dir("${env.WORKSPACE}/reports") {
+                archiveArtifacts(
+                    allowEmptyArchive: true,
+                    artifacts: 'blackbox-tests/**/*.log'
+                )
+            }
+            cleanWs()
+        }
+    }
+}
